ac16b0ef5f2195077b679033791a92791e619bcc
Eitaro Fukamachi <e.arrows@gmail.com>
2015-11-13 09:48:15 +0900

    Don't save failures in redqing.job. It's up to worker processes.

--- src/client/job.lisp
+++ src/client/job.lisp
@@ -11,16 +11,8 @@
 (defgeneric perform (job &rest args)
   (:method ((job job) &rest args)
     (declare (ignore args))
-    (error "PEFORM is not implemented for ~S" (class-name (class-of job))))
-  (:method :around ((job job) &rest args)
-    (declare (ignore args))
-    (handler-bind ((error
-                     (lambda (exception)
-                       (fail-job job exception))))
-      (call-next-method))))
+    (error "PEFORM is not implemented for ~S" (class-name (class-of job)))))
 
 (defgeneric fail-job (job exception)
   (:method ((job job) exception)
-    (vom:info "~A failed: ~A" job exception)
-    ;; TODO sava-failure
-    ))
+    (vom:info "~A failed: ~A" job exception)))
