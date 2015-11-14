aee1f4c4797ca01001fc923aa604ded11161fbf4
Eitaro Fukamachi <e.arrows@gmail.com>
2015-11-15 05:25:10 +0900

    Wait until worker manager stops.

--- src/worker.lisp
+++ src/worker.lisp
@@ -2,22 +2,31 @@
 (defpackage redqing.worker
   (:use #:cl)
   (:import-from #:redqing.worker.manager
-                #:make-manager)
+                #:make-manager
+                #:manager-children)
+  (:import-from #:redqing.worker.processor
+                #:processor-thread)
   (:import-from #:alexandria
                 #:ensure-list)
   (:export #:run
            #:stop
-           #:kill))
+           #:kill
+           #:wait-for-processors))
 (in-package :redqing.worker)
 
 (defun run (&key (host "localhost") (port 6379) (concurrency 25) (timeout 5)
               (queue "default"))
-  (redqing.worker.manager:start
-   (make-manager :host host
-                 :port port
-                 :queues (ensure-list queue)
-                 :count concurrency)
-   :timeout timeout))
+  (let ((manager (make-manager :host host
+                               :port port
+                               :queues (ensure-list queue)
+                               :count concurrency)))
+    (redqing.worker.manager:start manager :timeout timeout)
+    manager))
+
+(defun wait-for-processors (manager)
+  (map nil #'bt:join-thread
+       (mapcar #'processor-thread
+               (manager-children manager))))
 
 (defun stop (worker)
   (redqing.worker.manager:stop worker))
