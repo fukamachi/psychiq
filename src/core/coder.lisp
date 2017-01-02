afefdffa5aafd650af79ebac0249497b3d2b84b4
Eitaro Fukamachi <e.arrows@gmail.com>
2017-01-03 03:57:21 +0900

    Revert "Fix #1."
    This reverts commit 1d28c37f265a40a3ef75832d5dc653941f543819.

--- src/core/coder.lisp
+++ src/core/coder.lisp
@@ -10,12 +10,8 @@
            #:decode-object))
 (in-package :psychiq.coder)
 
-(defvar *encode-lock*
-  (bt:make-lock "encode-object"))
-
 (defun encode-object (object)
-  (bt:with-lock-held (*encode-lock*)
-    (jojo:to-json object :from :alist)))
+  (jojo:to-json object :from :alist))
 
 (defun decode-object (payload)
   (let ((jojo:*false-value* :false)
